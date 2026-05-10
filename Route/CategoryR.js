43d10b1f4f114ccbc0e3ff05522abc2c0d6c77eb
GUDDU KUMAR <122684135+Gudjrexl@users.noreply.github.com>
2026-05-11 00:39:00 +0530

    module fixed

--- Route/CategoryR.js
+++ Route/CategoryR.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Category = require('../schema/Category');
+const Category = require('../Schema/Category');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
